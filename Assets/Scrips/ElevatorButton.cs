45aa6ca0bdba0855fa46acfe30cfea73b98888bb
Nickael Vereza Bruzzi <qaznickael@hotmail.com>
2020-10-03 17:56:35 -0300

    add animation

---
+++ Assets/Scrips/ElevatorButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButton : MonoBehaviour
+{
+    public bool IsActive = false;
+    void OnTriggerEnter(Collider other)
+    {
+        IsActive = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        IsActive = false;
+    }
+}
